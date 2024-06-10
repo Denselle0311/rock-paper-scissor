71039f1a20edbd8015d2c7eb32094c82650407d7
Denselle0311 <144681965+Denselle0311@users.noreply.github.com>
2024-06-10 17:47:01 +0800

    add functionality for modal

--- script/script.js
+++ script/script.js
@@ -3,11 +3,12 @@ const para = document.querySelector('p');
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissor = document.getElementById('scissor'); 
-let buttons = document.querySelector('.container');
+const buttons = document.querySelector('.container');
+const roundInput = document.getElementById('rounds');
 let playerScore = 0;
 let computerScore = 0;
 let roundCount = 0;
-let buttonArr = [...buttons.querySelectorAll('button')];
+const buttonArr = [...buttons.querySelectorAll('button')];
 // console.log(buttonArr);
 buttonArr.forEach(btn => {
     btn.addEventListener('click',() => {
@@ -37,6 +38,39 @@ function moveResult(player,computer) {
         h1.textContent = 'You Loss!';
         computerScore++
     }
-    para.textContent = `${playerScore} WIN : ${computerScore} LOSS`;
-    roundCount++
+    roundInput.disabled = true;
+    roundCount++ 
+    para.textContent = `WIN :${playerScore} - ROUNDS: ${roundCount} - LOSS: ${computerScore}`;
+    if(roundCount === Number(roundInput.value)){
+        gameOver();
+    }
+}
+
+function gameOver() {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+    
+    modal.innerHTML = `<h1>!!! Game Over !!!</h1>
+                        <p>Your score: <span>${playerScore}</span></p>
+                        <button>Start new game</button>`;
+    document.body.appendChild(modal);
+    if(playerScore > computerScore) {
+       const h1 = document.querySelector('.modal h1');
+       h1.style.color = 'green';
+       h1.textContent = '!!! YOU WON !!!';
+    }
+    
+    const btn = document.querySelector('.modal button');
+    btn.addEventListener('click', () => {
+        h1.style.color = 'black';
+        h1.textContent = 'Rock Paper Scissor Game!';
+        para.textContent = 'choose a move';
+        roundInput.disabled = false;
+        roundInput.value = roundInput.defaultValue;
+        roundCount = 0;
+        playerScore = 0;
+        computerScore = 0;
+        para.textContent = `WIN :${playerScore} - ROUNDS: ${roundCount} - LOSS: ${computerScore}`;
+        document.body.removeChild(modal);
+    })
 }
